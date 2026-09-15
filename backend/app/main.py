95948723ed650effa779b9aac0fea1f8605e5ea3
=ankitsainifromjpr <=ankiitsani1403@gmail.com>
2026-09-15 08:24:57 +0530

    fix Cors error

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://study-buddyagent-5hmd.vercel.app",
+    "http://localhost:5173",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
